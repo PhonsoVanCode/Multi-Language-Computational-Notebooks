4da1c0f5b2de8df5620a2198831c0638984275e1
DNNYSTGMR <40438483+DNNYSTGMR@users.noreply.github.com>
2025-08-12 14:17:22 +0200

    update

--- javascript.js
+++ javascript.js
@@ -1,26 +1,66 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const params = new URLSearchParams(window.location.search);
-  const state = params.get("state");
-    console.log("State parameter:", state);
-    
-  });
-  
+let participantID;
 
+// common
 document.addEventListener("DOMContentLoaded", function() {
-    const startSurveyBtn = document.getElementById('startSurveyBtn');
+  participantID = new URLSearchParams(window.location.search).get("ParticipantID")
+});
+
+// index.html
+document.addEventListener("DOMContentLoaded", function() {
+
     const participantID = document.getElementById('participantID');
-    const surveyLink = document.getElementById('surveyLink');
+    const surveyButton1 = document.getElementById('surveyButton1');
+    const surveyLink1 = document.getElementById('surveyLink1');
 
-    if (startSurveyBtn && participantID && surveyLink) {
-        startSurveyBtn.onclick = function() {
+    if (surveyButton1 && surveyLink1 && participantID) {
+        surveyButton1.onclick = function() {
             const id = participantID.value.trim();
             if (id) {
                 const link = `https://migroup.qualtrics.com/jfe/form/SV_9QQGJz15kcDcfGe?ParticipantID=${encodeURIComponent(id)}`;
-                surveyLink.href = link;
-                surveyLink.click();
+                surveyLink1.href = link;
+                surveyLink1.click();
             } else {
                 alert('Please insert your ParticipantID');
             }
         };
     } 
+});
+
+// 1.html
+document.addEventListener("DOMContentLoaded", function() {
+
+    const surveyButton2 = document.getElementById('surveyButton2');
+    const surveyLink2 = document.getElementById('surveyLink2');
+
+    surveyButton2.onclick = function() {
+        surveyLink2.href = `https://migroup.qualtrics.com/jfe/form/SV_a3pk2o0B2n1lAuG?ParticipantID=${encodeURIComponent(participantID)}`;
+        surveyLink2.click();
+    } 
+    
+});
+
+// 2.html
+document.addEventListener("DOMContentLoaded", function() {
+
+    const surveyButton3 = document.getElementById('surveyButton3');
+    const surveyLink3 = document.getElementById('surveyLink3');
+
+    surveyButton3.onclick = function() {
+        surveyLink3.href = `https://migroup.qualtrics.com/jfe/form/SV_0j2gO9KztrHKJ0O?ParticipantID=${encodeURIComponent(participantID)}`;
+        surveyLink3.click();
+    } 
+    
+});
+
+// 3.html
+document.addEventListener("DOMContentLoaded", function() {
+
+    const surveyButton4 = document.getElementById('surveyButton4');
+    const surveyLink4 = document.getElementById('surveyLink4');
+
+    surveyButton4.onclick = function() {
+        surveyLink4.href = `https://migroup.qualtrics.com/jfe/form/SV_9X2uoov67AvuDf8?ParticipantID=${encodeURIComponent(participantID)}`;
+        surveyLink4.click();
+    } 
+    
 });
